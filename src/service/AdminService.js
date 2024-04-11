2c18d024353e2e572390632b9f2e84abdf081eb3
Igor <igor.leme.m@outlook.com>
2024-04-11 03:38:34 -0300

    Wip

--- src/service/AdminService.js
+++ src/service/AdminService.js
@@ -8,22 +8,20 @@ const getBestProfession = async (start, end) => {
     const endDate = DateTime.fromFormat(end, "dd-MM-yyyy");
 
     try {
-        const result =
-            getContractsWithPaidJobsInRange(startDate, endDate)
-                .then(contractsWithDataRange => {
-                    return contractsWithDataRange.map(contract => {
-                        const totalPrice = contract.Jobs.reduce((prev, curr) => {
-                            return prev + curr.price;
-                        }, 0);
-                        const client = contract.Client;
-                        return { paid: totalPrice, id: client.id, fullName: `${client.firstName} ${client.lastName}` };
-                    });
-                })
-                .then(result => {
-                    const best = result.sort((a, b) => b.paid - a.paid);
-                    return best.slice(0, 2);
+        return getContractsWithPaidJobsInRange(startDate, endDate)
+            .then(contractsWithDataRange => {
+                return contractsWithDataRange.map(contract => {
+                    const totalPrice = contract.Jobs.reduce((prev, curr) => {
+                        return prev + curr.price;
+                    }, 0);
+                    const client = contract.Client;
+                    return { paid: totalPrice, id: client.id, fullName: `${client.firstName} ${client.lastName}` };
                 });
-        return result;
+            })
+            .then(result => {
+                const best = result.sort((a, b) => b.paid - a.paid);
+                return best.slice(0, 2);
+            });
     } catch (error) {
         throw new Error('Error fetching best profession: ' + error.message);
     }
